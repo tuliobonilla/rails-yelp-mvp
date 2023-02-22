fda28e2ab9a3e3ec454ecaa8915bd411f8786286
tulio bonilla <tuliobonilla@gmail.com>
2023-02-22 15:41:11 +0100

    YELP MVP without proper HTML and views

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,28 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(set_restaurant)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_restaurant
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Deleting all existing restaurants'
+Restaurant.destroy_all
+puts 'Filling the database'
+20.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+end
+all_restaurants = Restaurant.all
+all_restaurants.each do |restaurant|
+  rand(5..10).times do
+    Review.create(
+      content: Faker::Restaurant.review,
+      rating: rand(0..5),
+      restaurant: restaurant
+    )
+  end
+end
+puts "Created #{Restaurant.count} Restaurants and #{Review.count} Reviews"
